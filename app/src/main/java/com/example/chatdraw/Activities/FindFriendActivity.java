bca1a63bfd3c9c31f5ab54f73abe562a0f353f38
jo <joannasarasusan@gmail.com>
2019-07-10 18:59:42 +0700

    Change FindFriendActivity layout

--- app/src/main/java/com/example/chatdraw/Activities/FindFriendActivity.java
+++ app/src/main/java/com/example/chatdraw/Activities/FindFriendActivity.java
@@ -14,9 +14,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.chatdraw.Adapters.NewFriendAdapter;
@@ -31,6 +33,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -38,29 +41,38 @@ public class FindFriendActivity extends AppCompatActivity {
 
     private static String TAG = "FindFriendActivity";
 
+    private ImageView newImageView;
+    private TextView newTextView;
+    private Button newButton;
+    private String newUsername;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friend);
 
-        super.onStart();
-        // Create a custom adapter for the friend list ListView
-        final NewFriendAdapter newFriendAdapter = new NewFriendAdapter(this);
+        // get the views
+        newImageView = findViewById(R.id.find_friend_newImageView);
+        newTextView = findViewById(R.id.find_friend_newTextView);
+        newButton = findViewById(R.id.find_friend_newButton);
 
-        // create an onItemClickListener for the ListView
-        AdapterView.OnItemClickListener onItemClickListener
-                = new AdapterView.OnItemClickListener() {
+        // set as invisible
+        newImageView.setVisibility(View.INVISIBLE);
+        newTextView.setVisibility(View.INVISIBLE);
+        newButton.setVisibility(View.INVISIBLE);
+
+        // set onClickListener on the button
+        // if clicked, go back to FriendListActivity
+        // put extra containing the name of the clicked profile
+        newButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // get clicked item
-                NewFriendItem newFriendItem = (NewFriendItem) newFriendAdapter.getItem(position);
-
+            public void onClick(View v) {
                 // create Intent to send selected friend's name back to FriendListActivity
                 final Intent intent = new Intent();
 
                 // get new friend's uID, add to intent, and then destroy this activity
                 FirebaseFirestore.getInstance().collection("Users")
-                        .whereEqualTo("username", newFriendItem.getUsername())
+                        .whereEqualTo("username", newUsername)
                         .get()
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
@@ -118,13 +130,7 @@ public class FindFriendActivity extends AppCompatActivity {
                             }
                         });
             }
-        };
-
-        // set onClickListener on the listView
-        // if clicked, go back to FriendListActivity
-        // put extra containing the name of the clicked profile
-        ListView listView = findViewById(R.id.find_friend_listview);
-        listView.setOnItemClickListener(onItemClickListener);
+        });
 
         // set the action bar
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
@@ -137,11 +143,10 @@ public class FindFriendActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newFriendAdapter.clearData();
                 String str = editText.getText().toString();
                 if (!str.trim().equals("")) {
                     // find user and update listview
-                    findUserInDatabase(newFriendAdapter, str.trim());
+                    findUserInDatabase(str.trim());
                 }
             }
         });
@@ -154,7 +159,7 @@ public class FindFriendActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    public void findUserInDatabase(final NewFriendAdapter newFriendAdapter, String inputText) {
+    public void findUserInDatabase(String inputText) {
         // Check if the input start with @
         final String text;
         if (inputText.charAt(0) != '@') {
@@ -165,10 +170,6 @@ public class FindFriendActivity extends AppCompatActivity {
 
         if (text.length() < 2) return;
 
-        // get users whose usernames contain the inputted text
-        char firstChar = text.charAt(1);
-        char nextChar = ++firstChar;
-
         FirebaseFirestore.getInstance().collection("Users")
                 .whereEqualTo("username", text)
                 .get()
@@ -176,17 +177,31 @@ public class FindFriendActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
-                            newFriendAdapter.clearData();
-                            int count = 0;
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                if (count > 20) break;
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                count++;
-                                updateListView(
-                                        newFriendAdapter,
-                                        document.getString("name"),
-                                        document.getString("username"),
-                                        document.getString("imageUrl"));
+
+                                String name = document.getString("name");
+                                newUsername = document.getString("username");
+                                String imgUrl = document.getString("imageUrl");
+
+                                if (name == null) {
+                                    name = "Anonymous";
+                                    newTextView.setTextColor(getResources().getColor(R.color.pLight));
+                                }
+                                newTextView.setText(name);
+                                if (imgUrl != null) {
+                                    Picasso.get()
+                                            .load(imgUrl)
+                                            .fit()
+                                            .into(newImageView);
+                                } else {
+                                    newImageView.setImageResource(R.drawable.blank_account);
+                                }
+
+                                newImageView.setVisibility(View.VISIBLE);
+                                newTextView.setVisibility(View.VISIBLE);
+                                newButton.setVisibility(View.VISIBLE);
+
                             }
                         } else {
                             Log.w(TAG, "Error getting documents.", task.getException());
@@ -201,18 +216,5 @@ public class FindFriendActivity extends AppCompatActivity {
         // if the back button is pressed, go back to previous activity
         finish();
         return true;
-    }
-
-    public void updateListView(NewFriendAdapter newFriendAdapter, String name,
-                               String username, String imageUrl) {
-        // find the friend list ListView
-        ListView listView = findViewById(R.id.find_friend_listview);
-
-        // Instantiate a new NewFriendItem and add it to the custom adapter
-        NewFriendItem newFriendItem = new NewFriendItem(name, username, imageUrl);
-        newFriendAdapter.addAdapterItem(newFriendItem);
-
-        // set the adapter to the ListView
-        listView.setAdapter(newFriendAdapter);
     }
 }
