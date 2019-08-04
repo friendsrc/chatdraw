1cae7ade994790be73257ef513f7998519aa776c
jo <joannasarasusan@gmail.com>
2019-08-04 16:06:19 +0800

    Create activity

---
+++ app/src/main/java/com/example/chatdraw/Activities/DrawActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatdraw.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatdraw.R;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw);
+    }
+}
